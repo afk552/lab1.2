3adb82989b7b1ff0fa54a6337a4345c201de5f41
afk552 <ez552@mail.ru>
2022-09-24 01:45:27 +0300

    Added more protection

--- reverse_diff.py
+++ reverse_diff.py
@@ -1,23 +1,25 @@
-chislo = (input("Type a number: "))
-if (chislo.isdigit() == True):
-    a = int(chislo)
-    mas = []
-    strr = ''
+while True:
+    try:
+        chislo = (input("Type a number: "))
+        if (chislo.isdigit() == True):
+            a = int(chislo)
+        mas = []
+        strr = ''
 
-    while a > 0:
-        mas.append(a%10)
-        a = a // 10
-    print ("Reversed typed number: ", end = '')
-    for i in range (len(mas)):
-        print (mas[i], end = '')
+        while a > 0:
+            mas.append(a%10)
+            a = a // 10
+        print ("Reversed typed number: ", end = '')
+        for i in range (len(mas)):
+            print (mas[i], end = '')
 
-    for i in range (len(mas)):
-        strr = strr + str(mas[i])
-    print ("\n")
+        for i in range (len(mas)):
+            strr = strr + str(mas[i])
+        print ("\n")
 
-    reversd = int(strr)
-    result = abs(reversd - int(chislo))
-    print ("Result:", result)
-
-else:
-    print ("You typed not a number -_-")
+        reversd = int(strr)
+        result = abs(reversd - int(chislo))
+        print ("Result:", result)
+        break
+    except:
+        print("It looks like you made a mistake, you must write the number! ¯\_(ツ)_/¯")
