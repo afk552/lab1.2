ae048064862e6947f09130fef92dc5fa8dc923e4
afk552 <ez552@mail.ru>
2022-09-24 01:21:44 +0300

    Explanation added

--- reverse_diff.py
+++ reverse_diff.py
@@ -1,15 +1,18 @@
-chislo = int(input())
+chislo = int(input("Type a number: "))
 a = chislo
 mas = []
 strr = ''
 while a > 0:
     mas.append(a%10)
     a = a // 10
-print (mas)
+print ("Reversed typed number: ", end = '')
+for i in range (len(mas)):
+    print (mas[i], end = '')
 
 for i in range (len(mas)):
     strr = strr + str(mas[i])
+print ("\n")
 
 reversd = int(strr)
 result = abs(reversd - chislo)
-print (result)
+print ("Result:", result)
