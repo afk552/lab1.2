0b3025841be3946771bfeb8444824b071c1272df
afk552 <ez552@mail.ru>
2022-09-24 01:38:22 +0300

    Added letter protection

--- reverse_diff.py
+++ reverse_diff.py
@@ -1,18 +1,23 @@
-chislo = int(input("Type a number: "))
-a = chislo
-mas = []
-strr = ''
-while a > 0:
-    mas.append(a%10)
-    a = a // 10
-print ("Reversed typed number: ", end = '')
-for i in range (len(mas)):
-    print (mas[i], end = '')
+chislo = (input("Type a number: "))
+if (chislo.isdigit() == True):
+    a = int(chislo)
+    mas = []
+    strr = ''
 
-for i in range (len(mas)):
-    strr = strr + str(mas[i])
-print ("\n")
+    while a > 0:
+        mas.append(a%10)
+        a = a // 10
+    print ("Reversed typed number: ", end = '')
+    for i in range (len(mas)):
+        print (mas[i], end = '')
 
-reversd = int(strr)
-result = abs(reversd - chislo)
-print ("Result:", result)
+    for i in range (len(mas)):
+        strr = strr + str(mas[i])
+    print ("\n")
+
+    reversd = int(strr)
+    result = abs(reversd - int(chislo))
+    print ("Result:", result)
+
+else:
+    print ("You typed not a number -_-")
