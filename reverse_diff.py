38f10c0cd539f6c9e6f5b062b6937b2bf4184815
afk552 <ez552@mail.ru>
2022-09-24 02:03:58 +0300

    Added count function

--- reverse_diff.py
+++ reverse_diff.py
@@ -1,25 +1,30 @@
+def revcalc(result):
+    a = int(chislo)
+    mas = []
+    strr = ''
+
+    while a > 0:
+        mas.append(a%10)
+        a = a // 10
+    print ("Reversed typed number: ", end = '')
+    for i in range (len(mas)):
+        print (mas[i], end = '')
+
+    for i in range (len(mas)):
+        strr = strr + str(mas[i])
+    print ("\n")
+
+    reversd = int(strr)
+    result = abs(reversd - int(chislo))
+    return result
+
 while True:
     try:
         chislo = (input("Type a number: "))
         if (chislo.isdigit() == True and chislo[0]!="0"):
-            a = int(chislo)
-        mas = []
-        strr = ''
-
-        while a > 0:
-            mas.append(a%10)
-            a = a // 10
-        print ("Reversed typed number: ", end = '')
-        for i in range (len(mas)):
-            print (mas[i], end = '')
-
-        for i in range (len(mas)):
-            strr = strr + str(mas[i])
-        print ("\n")
-
-        reversd = int(strr)
-        result = abs(reversd - int(chislo))
-        print ("Result:", result)
-        break
+            print ("Result:", revcalc(chislo))
+            break
+        else:
+            print("It looks like you made a mistake, you must write the number! ¯\_(ツ)_/¯")
     except:
-        print("It looks like you made a mistake, you must write the number! ¯\_(ツ)_/¯")
+        print("Error!")
