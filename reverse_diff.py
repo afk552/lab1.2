4afce88f3a53c2bdc20b1ae76bd002443b0bf2c4
afk552 <ez552@mail.ru>
2022-09-24 02:10:21 +0300

    Added a guide for user

--- reverse_diff.py
+++ reverse_diff.py
@@ -6,6 +6,7 @@ def revcalc(result):
     while a > 0:
         mas.append(a%10)
         a = a // 10
+
     print ("Reversed typed number: ", end = '')
     for i in range (len(mas)):
         print (mas[i], end = '')
@@ -20,6 +21,7 @@ def revcalc(result):
 
 while True:
     try:
+        print ("This program reverse your number and count the difference between your number and reversed one.")
         chislo = (input("Type a number: "))
         if (chislo.isdigit() == True and chislo[0]!="0"):
             print ("Result:", revcalc(chislo))
